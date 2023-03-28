4ec77144b65ac1a61ab0accd219356e9876a680e
Rusy Richter <rusyrichter@gmail.com>
2023-03-27 23:44:41 -0400

    Add project files.

---
+++ class_3_27/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using class_3_27.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace class_3_27.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }       
+    }
+}
